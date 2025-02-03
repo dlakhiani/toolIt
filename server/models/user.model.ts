08d49b20a2880d8f2e688b41cc17ddc0c8d8441b
dlakhiani <dlakhiani02@gmail.com>
2025-02-02 20:05:02 -0500

    include password on user + hash on save

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,13 +1,17 @@
 import mongoose, { HydratedDocument, Model, Schema } from "mongoose"
+import { compare, genSalt, hash } from "bcrypt"
 
 interface IUser {
     name: string
     email: string
+    passwordHash: string
+    loginAt: Date
     createdAt: Date
 }
 
 interface IUserMethods {
     getGreeting(): string
+    comparePasswordHash(passwordHash: string): Promise<boolean>
 }
 
 interface IUserModel extends Model<IUser, IUserMethods> {
@@ -27,17 +31,46 @@ const userSchema = new Schema(
             unique: true,
             lowercase: true,
         },
+        passwordHash: {
+            type: String,
+            required: true,
+        },
+        loginAt: {
+            type: Date,
+        },
     },
     {
-        // Adds createdAt and updatedAt timestamps
+        // adds createdAt and updatedAt timestamps
         timestamps: true,
     }
 )
 
+// model hooks
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("passwordHash")) return next()
+
+    try {
+        const salt = await genSalt()
+        this.passwordHash = await hash(this.passwordHash, salt)
+        next()
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            next(err)
+        } else {
+            next(new Error("An unknown error occurred while hashing the password"))
+        }
+    }
+})
+
+// instance methods
 userSchema.method("getGreeting", function getGreeting(): string {
     return `Hello, ${this.name}!`
 })
+userSchema.method("comparePasswordHash", async function comparePasswordHash(passwordHash: string): Promise<boolean> {
+    return compare(passwordHash, this.passwordHash)
+})
 
+// static methods
 userSchema.static("findByEmail", function findByEmail(email: string) {
     return this.findOne({ email })
 })
